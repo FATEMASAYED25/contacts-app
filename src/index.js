6bcff3f0bc42fb3b914bd3fad0cd05486335b383
FATEMASAYED25 <batasayed25@gmail.com>
2026-05-25 15:03:05 +0300

    developing contact app

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';    
+import { HashRouter } from 'react-router-dom';    
 
 
 ReactDOM.render(
 
-    <BrowserRouter basename="/contacts-app">
+    <HashRouter>
     
     <App />
-    </BrowserRouter>
+    </HashRouter>
 
 
 , document.getElementById('root'));
