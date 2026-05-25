0ca80eaf98f39fa87227e83cb04c3a6b8cbf1bef
FATEMASAYED25 <batasayed25@gmail.com>
2026-05-25 14:14:22 +0300

    Updates

---
+++ precache-manifest.707c77d67fd41723952871dbd01c5c77.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "14dabbdcf7570e1c20fd",
+    "url": "/contacts-app/static/css/main.3bb4c30e.chunk.css"
+  },
+  {
+    "revision": "14dabbdcf7570e1c20fd",
+    "url": "/contacts-app/static/js/main.14dabbdc.chunk.js"
+  },
+  {
+    "revision": "e76cdbe5b995e2aca2e9",
+    "url": "/contacts-app/static/js/1.e76cdbe5.chunk.js"
+  },
+  {
+    "revision": "5752e206956e7a71b497",
+    "url": "/contacts-app/static/js/runtime~main.5752e206.js"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/contacts-app/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "1456a6be906dcfe7818c47b28b409501",
+    "url": "/contacts-app/static/media/person.1456a6be.svg"
+  },
+  {
+    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
+    "url": "/contacts-app/static/media/search.0fc053ba.svg"
+  },
+  {
+    "revision": "c9c2fd159a339e59c77962878b1676bd",
+    "url": "/contacts-app/static/media/person-add.c9c2fd15.svg"
+  },
+  {
+    "revision": "576f26ff09a3275d9da0621e6b34898d",
+    "url": "/contacts-app/static/media/cancel.576f26ff.svg"
+  },
+  {
+    "revision": "26982edcbdefb48cf7124c50f957592f",
+    "url": "/contacts-app/index.html"
+  }
+];
