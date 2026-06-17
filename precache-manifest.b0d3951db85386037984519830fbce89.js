abc9142bf1870e7f1eb96c12ec757c86d47f38cf
FATEMASAYED25 <batasayed25@gmail.com>
2026-06-17 13:15:26 +0300

    Updates

--- precache-manifest.b0d3951db85386037984519830fbce89.js
+++ precache-manifest.b0d3951db85386037984519830fbce89.js
@@ -16,20 +16,20 @@ self.__precacheManifest = [
     "url": "/contacts-app/static/js/runtime~main.5752e206.js"
   },
   {
-    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
-    "url": "/contacts-app/static/media/arrow-back.c0c5c509.svg"
-  },
-  {
     "revision": "1456a6be906dcfe7818c47b28b409501",
     "url": "/contacts-app/static/media/person.1456a6be.svg"
   },
   {
-    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
-    "url": "/contacts-app/static/media/search.0fc053ba.svg"
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/contacts-app/static/media/arrow-back.c0c5c509.svg"
   },
   {
     "revision": "c9c2fd159a339e59c77962878b1676bd",
     "url": "/contacts-app/static/media/person-add.c9c2fd15.svg"
+  },
+  {
+    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
+    "url": "/contacts-app/static/media/search.0fc053ba.svg"
   },
   {
     "revision": "576f26ff09a3275d9da0621e6b34898d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/contacts-app/precache-manifest.219ba5ac193d0f08d5c6408fc2672452.js"
+  "/contacts-app/precache-manifest.b0d3951db85386037984519830fbce89.js"
 );
 
 workbox.clientsClaim();
