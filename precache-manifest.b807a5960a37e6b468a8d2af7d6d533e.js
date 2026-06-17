d2bb3ad8869c4434cbed0c015fe51b4c84c3e467
FATEMASAYED25 <batasayed25@gmail.com>
2026-06-17 14:06:25 +0300

    Updates

--- precache-manifest.b807a5960a37e6b468a8d2af7d6d533e.js
+++ precache-manifest.b807a5960a37e6b468a8d2af7d6d533e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b14b895a52c1077946a8",
+    "revision": "331b8c3aae68d565f3bd",
     "url": "/contacts-app/static/css/main.3bb4c30e.chunk.css"
   },
   {
-    "revision": "b14b895a52c1077946a8",
-    "url": "/contacts-app/static/js/main.b14b895a.chunk.js"
+    "revision": "331b8c3aae68d565f3bd",
+    "url": "/contacts-app/static/js/main.331b8c3a.chunk.js"
   },
   {
     "revision": "7f75162c5a6594f67cc8",
@@ -20,6 +20,10 @@ self.__precacheManifest = [
     "url": "/contacts-app/static/media/person.1456a6be.svg"
   },
   {
+    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
+    "url": "/contacts-app/static/media/search.0fc053ba.svg"
+  },
+  {
     "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
     "url": "/contacts-app/static/media/arrow-back.c0c5c509.svg"
   },
@@ -28,15 +32,11 @@ self.__precacheManifest = [
     "url": "/contacts-app/static/media/person-add.c9c2fd15.svg"
   },
   {
-    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
-    "url": "/contacts-app/static/media/search.0fc053ba.svg"
-  },
-  {
     "revision": "576f26ff09a3275d9da0621e6b34898d",
     "url": "/contacts-app/static/media/cancel.576f26ff.svg"
   },
   {
-    "revision": "10b54fd4753bf1f963cfbd3f35186ca9",
+    "revision": "2a87b4755f518374dcefa244b0aaa1fb",
     "url": "/contacts-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/contacts-app/precache-manifest.b0d3951db85386037984519830fbce89.js"
+  "/contacts-app/precache-manifest.b807a5960a37e6b468a8d2af7d6d533e.js"
 );
 
 workbox.clientsClaim();
